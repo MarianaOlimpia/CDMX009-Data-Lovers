7beca9cec609fa774043f674641f19bade231cc6
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-03-31 20:46:44 -0600

    test al 100%

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,10 +1,24 @@
 // importamos la función `example`
-import { filterByTitle, getAllNews } from '../src/data';
+import { filterByTitle, getAllNews, filterNewsByFeedname } from '../src/data';
 
 // Prueba de filtro por título para input
-describe(filterByTitle, () => {
+describe('filterByTitle', () => {
   test('Debería retornar el título que incluya la palabra Halloween', () => {
-    expect('All of Halloween is happening in TF2 s Scream Fortress X').toMatch('Halloween');
+    expect(filterByTitle('operation')).toEqual([
+      {
+        gid: '3316234163932872071',
+        title: 'Operation Canteen Crasher!',
+        img: 'https://steamcdn-a.akamaihd.net/steam/news/46099/canteen_crasher_event.jpg?t=1496190709.jpg',
+        url: 'https://steamstore-a.akamaihd.net/news/externalpost/tf2_blog/3316234163932872071',
+        is_external_url: true,
+        author: '',
+        contents: '<a href="https://steamcommunity.com/groups/potatomvmservers#announcements/detail/2533733726690659777"> </a> <a href="https://steamcommunity.com/groups/potatomvmservers#announcements/detail/2533733726690659777" target="_blank">Operation Canteen Crasher</a> is a new community Mann vs. Machine event brought you by the same group that hosted <a href="https://steamcommunity.com/groups/potatomvmservers#announcements/detail/1663388904536296299" target="_blank">Operation Titanium Tank</a>! This community-created event features 28 maps with 50 missions and new shiny medals! Complete the campaign and take home up to 5 in-game Canteen Crasher participan...',
+        feedlabel: 'TF2 Blog',
+        date: 1542738000,
+        feedname: 'tf2_blog',
+        feed_type: 0,
+        appid: 440,
+      }]);
   });
 });
 
@@ -150,5 +164,26 @@ describe('getAllNews', () => {
       feed_type: 0,
       appid: 440,
     }]);
+  });
+});
+
+describe('filterNewsByFeedname', () => {
+  test('deberia devolver los titulos con feedname rps', () => {
+    expect(filterNewsByFeedname('rps')).toEqual([
+      {
+        gid: '2403126706534048982',
+        title: 'All of Halloween is happening in TF2 s Scream Fortress X',
+        url: 'https://steamstore-a.akamaihd.net/news/externalpost/rps/2403126706534048982',
+        is_external_url: true,
+        img: 'https://assets.rockpapershotgun.com/images/2018/10/ScreamFortressX.jpg/RPSS/resize/760x-1/format/jpg/quality/90',
+        author: 'contact@rockpapershotgun.com (Matt Cox)',
+        contents: 'The thing about special Halloween game modes is that they&#8217;re fun for a round or two, then the novelty wears off. The thing about <a href="https://www.rockpapershotgun.com/game/team-fortress-2/">Team Fortress 2</a>&#8216;s Halloween mode is that there are 18 of them. Ok, so only 5 of those are new &#8211; but you can also play the 13 previous Halloween events, w...',
+        feedlabel: 'Rock, Paper, Shotgun',
+        date: 1540295862,
+        feedname: 'rps',
+        feed_type: 0,
+        appid: 440,
+      },
+    ]);
   });
 });
