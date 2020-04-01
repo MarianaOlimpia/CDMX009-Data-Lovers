e4d717608ae31d9e4a104fe01b26b2b5c0e31ffe
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-03-31 20:22:58 -0600

    funcion filterNewsByFeedname ya funciona

--- src/data.js
+++ src/data.js
@@ -2,13 +2,11 @@
 import data from './data/steam/steam.js';
 
 // // Funciones
-
 // Función para filtrar por título
 export function filterByTitle(title) {
   // eslint-disable-next-line max-len
   return data.appnews.newsitems.filter((titulo) => titulo.title.toLowerCase().includes(title.toLowerCase()));
 }
-// console.log(filterByTitle);
 
 // Función para traer todos los títulos.
 export function getAllNews() {
@@ -18,29 +16,4 @@ export function getAllNews() {
 // Función para traer los títulos por feedname.
 export function filterNewsByFeedname(categoryId) {
   return data.appnews.newsitems.filter((news) => news.feedname === categoryId);
-}
-
-// Función para traer sólo las noticias de "Tf2 Blog".
-export function getSteamBlog() {
-  return data.appnews.newsitems.filter((steam) => steam.feedname === 'tf2_blog');
-}
-
-// Función para traer sólo las noticias de la categoría "Pc Gamer".
-export function getPcGamer() {
-  return data.appnews.newsitems.filter((pcGamer) => pcGamer.feedname === 'pcgamer');
-}
-
-// Función para traer sólo las noticias de la categoría "Product Updates".
-export function getProductUpdates() {
-  return data.appnews.newsitems.filter((productUp) => productUp.feedname === 'steam_updates');
-}
-
-// Función para traer sólo las noticias de la categoría "Eurogamer".
-export function getEurogamer() {
-  return data.appnews.newsitems.filter((eurogamer) => eurogamer.feedname === 'eurogamer');
-}
-
-// Función para traer sólo las noticias de la categoría "Rock, Paper, Shotgun".
-export function getRockPaperShotgun() {
-  return data.appnews.newsitems.filter((rps) => rps.feedname === 'rps');
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // Importamos la función filterBYTitle (que filtra por título) del archivo data.js al main.js
 import {
-  // eslint-disable-next-line max-len
-  filterByTitle, getAllNews, getSteamBlog, getPcGamer, getProductUpdates, getEurogamer, getRockPaperShotgun, filterNewsByFeedname
+  filterByTitle, getAllNews, filterNewsByFeedname,
 } from './data.js';
 import data from './data/steam/steam.js';
 
@@ -9,54 +8,18 @@ import data from './data/steam/steam.js';
 const newsPanel = document.querySelector('#newsPanel');
 // 'let input' trae el input del DOM en el cual tiene el id #wordSearch.
 const input = document.querySelector('#wordSearch');
-
 // 'a' trae el enlace que corresponde al botón de buscar en el DOM id #elClick.
 const a = document.querySelector('#elClick');
-
 // let showNews corresponde a la sección en HTML donde se mostrarán las listas de noticias.
 const showNews = document.querySelector('.showNews');
-
 // let lista es la variable que va a contener los arrays.
 let lista = [];
-
-// Antiguos Nodos para Funciones Individuales.
-// // let catAllNews trae el enlace que corresponde a la categoría "Todas las categorías".
-// const catAllNews = document.querySelector('#allTheNews');
-
-// // const catTF2blog trae el enlace que corresponde a la categoría "Steam Blog".
-// const catTF2blog = document.querySelector('#tf2_blog');
-
-// // const catTF2blog trae el enlace que corresponde a la categoría "Pc Gamer".
-// const catPcGamer = document.querySelector('#pcgamer');
-
-// // const catProductUpdates trae el enlace que corresponde a la categoría "Product Updates".
-// const catProductUpdates = document.querySelector('#steam_updates');
-
-// // const catProductUpdates trae el enlace que corresponde a la categoría "Eurogamer".
-// const catEurogamer = document.querySelector('#eurogamer');
-
-// // const catProductUpdates trae el enlace que corresponde a la categoría "Rock, Paper, Shotgun".
-// const catRPS = document.querySelector('#rps');
-
+// let catAllNews trae el enlace que corresponde a la categoría "Todas las categorías".
+const catAllNews = document.querySelector('#allTheNews');
 // Nodo para traer todas las categorías
 const category = document.querySelectorAll('.categories');
 
-// Nodo para guardar la categoría seleccionada por el usuario.
-let filteredNews = [];
-
-// // Eventos
-// Agrega el evento 'click' al enlace que corresponde al botón de búsqueda.
-a.addEventListener('click', getInputSearch);
-catAllNews.addEventListener('click', getCatAllNews);
-catTF2blog.addEventListener('click', getSteamNews);
-catPcGamer.addEventListener('click', getPcGamerNews);
-catProductUpdates.addEventListener('click', getProductUpdatesNews);
-catEurogamer.addEventListener('click', getEurogamerNews);
-catRPS.addEventListener('click', getRPSNews);
-
-
 // //Funciones
-
 // Oculta el panel de novedades para mostrar el espacio donde se imprime la lista de noticias.
 const showAndHide = () => {
   newsPanel.style.display = 'none';
@@ -89,53 +52,22 @@ const getCatAllNews = () => {
   showAndHide();
 };
 
-// // Obtiene la lista de noticias al seleccionar la categoría "Steam Blog".
-// const getSteamNews = () => {
-//   lista = getSteamBlog();
-//   // console.log(lista);
-//   showAndHide();
-// };
-
-// // Obtiene la lista de noticias al seleccionar la categoría "Pc Gamer".
-// const getPcGamerNews = () => {
-//   lista = getPcGamer();
-//   // console.log(lista);
-//   showAndHide();
-// };
-
-// // Obtiene la lista de noticias al seleccionar la categoría "Product Updates".
-// const getProductUpdatesNews = () => {
-//   lista = getProductUpdates();
-//   // console.log(lista)
-//   showAndHide();
-// };
-
-// // Obtiene la lista de noticias al seleccionar la categoría "Eurogamer".
-// const getEurogamerNews = () => {
-//   lista = getEurogamer();
-//   // console.log(lista)
-//   showAndHide();
-// };
-
-// // Obtiene la lista de noticias al seleccionar la categoría "Rock, Paper, Shotgun".
-// const getRPSNews = () => {
-//   lista = getRockPaperShotgun();
-//   // console.log(lista)
-//   showAndHide();
-// };
-
 // Función que genera el array según la categoría electa en Submenú de Categorías.
 for (let i = 0; i < category.length; i += 1) {
+  // eslint-disable-next-line no-loop-func
   category[i].addEventListener('click', () => {
-    let categoryId = category[i].id;
-    filteredNews = filterNewsByFeedname(categoryId);
-    console.log(filteredNews) // 1 son correctas // 2 tunear el show and hide (showNews) 3.-  Usar la lista para dibujar las noticias
-    showAndHide(filteredNews);
+    const categoryId = category[i].id;
+    lista = filterNewsByFeedname(categoryId);
+    showAndHide();
   });
 }
 
+// // Eventos
+// Agrega el evento 'click' al enlace que corresponde al botón de búsqueda.
+a.addEventListener('click', getInputSearch);
+catAllNews.addEventListener('click', getCatAllNews);
 
-// Imprimiendo noticias en Panel de Novedades. 
+// Imprimiendo noticias en Panel de Novedades.
 newsPanel.innerHTML = `
 <div id="noticiaPrincipal">
 <a href="${data.appnews.newsitems[0].url}" title="${data.appnews.newsitems[0].title}"  target="_blank"><img src="${data.appnews.newsitems[0].img}" class="principal"></a>
